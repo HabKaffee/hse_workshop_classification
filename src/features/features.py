2e2ba2d90f26fec28b455411dce1366c2c43816d
HabKaffee <neknaum1@gmail.com>
2022-10-16 20:32:48 +0300

    Modeling done + Pipeline implemented

--- src/features/features.py
+++ src/features/features.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
+def get_hours(time:str) -> int:
+    return int(time.split(':')[0])
 
 def add_early_wakeup(df: pd.DataFrame) -> pd.DataFrame:
-    pass
+    df['Жаворонок'] = df['Время пробуждения']
+    df['Жаворонок'] = df['Время пробуждения'].apply(lambda x: 1 if get_hours(x) <= 7 else 0)
     return df
